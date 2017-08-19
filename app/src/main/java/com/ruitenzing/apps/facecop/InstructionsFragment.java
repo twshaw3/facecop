b6cb35c037813fda53220f43ecaa93a4d4b7d5b6
twshaw3 <twshaw3@gmail.com>
2017-08-19 15:40:52 -0700

    prototype1

---
+++ app/src/main/java/com/ruitenzing/apps/facecop/InstructionsFragment.java
@@ -0,0 +1,27 @@
+package com.ruitenzing.apps.facecop;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InstructionsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.open_camera_instrucation, container, false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/ruitenzing/apps/facecop/MainActivity.java
+++ app/src/main/java/com/ruitenzing/apps/facecop/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ruitenzing.apps.facecop;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +12,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_IMAGE_PICK_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button verifyButton = (Button) findViewById(R.id.verify_button);
+        verifyButton.setOnClickListener(createVerifyCLickListener());
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +36,42 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == GALLERY_IMAGE_PICK_REQUEST_CODE
+                    && resultCode == RESULT_OK
+                    && data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver()
+                        .query(
+                                selectedImage,
+                                filePathColumn,
+                                null, null, null);
+                cursor.moveToFirst();
+                int filePathColumnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String filePath = cursor.getString(filePathColumnIndex);
+                Toast.makeText(this, filePath, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "FaceCop didn't get the picked picture. :(. Try again!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private View.OnClickListener createVerifyCLickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryINtent = new Intent(
+                        Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryINtent,GALLERY_IMAGE_PICK_REQUEST_CODE);
+            }
+        };
     }
 
     @Override
